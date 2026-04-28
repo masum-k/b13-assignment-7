a060ca0c165ee0d4df68c1e122ba9acc0428c8fd
masum-k <maasumkamal@gmail.com>
2026-04-28 23:04:15 +0600

    changed toastify position

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
           </main>
           <Footer />
         </Providers>
-        <ToastContainer />
+        <ToastContainer position="bottom-right"/>
       </body>
     </html>
   );
